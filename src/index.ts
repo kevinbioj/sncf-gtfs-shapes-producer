5ad104e95730bbe156e28dc74363b78b1efe7ea2
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-04-05 16:23:42 +0200

    fix: able to customise output file

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { writeResource } from "./gtfs/write-resource";
 
 const feedUrl =
   "https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip";
-const outputFile = "/Users/kevin/sncf-ler.zip";
+const outputFile = process.env.OUTPUT_FILE ?? "~/sncf-ler-gtfs.zip";
 const routeId = "FR:Line::325702f0-8067-4665-a0d8-5ebce7e59d0a:";
 
 console.log("== LER GTFS Maker ==");
